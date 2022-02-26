e3e41ce0566d7ddb9d9232b984dbdd229222f4c1
Nithin Krishna <nmnithinkrishna@gmail.com>
2022-02-27 00:15:23 +0530

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CustomCategoryJObject
+{
+    public class Category
+    {
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+        public Category[] subs { get; set; }
+    }
+}
